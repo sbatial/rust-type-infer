7cb7590932284af4a6bc6823fd5639ad4b1ff922
sbatial <mail@kbode.com>
2023-06-23 01:41:47 +0200

    add example with nested functions and result propagation (types missing)

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::{num::ParseFloatError, str::FromStr};
+
+fn main() {
+    println!("{}", wrap_parsing().unwrap());
+}
+
+fn wrap_parsing() {
+    let u_in = get_user_input();
+    let u_num = parse_user_input::<f32>(u_in);
+    return u_num;
+}
+
+fn get_user_input() {
+    return String::from("32.0");
+}
+
+fn parse_user_input<T: FromStr>(input: String) {
+    return input.parse::<T>();
+}
