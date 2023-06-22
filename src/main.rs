90a16b91133a58594610b094c1389a9058515a59
sbatial <mail@kbode.com>
2023-06-23 01:45:08 +0200

    add types via rust-analyzer help

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use std::str::FromStr;
+use std::{str::FromStr, num::ParseFloatError};
 
 fn main() {
     println!("{}", wrap_parsing().unwrap());
 }
 
-fn wrap_parsing() {
+fn wrap_parsing() -> Result<f32, ParseFloatError> {
     let u_in = get_user_input();
     let u_num = parse_user_input::<f32>(u_in);
     return u_num;
 }
 
-fn get_user_input() {
+fn get_user_input() -> String {
     return String::from("32.0");
 }
 
-fn parse_user_input<T: FromStr>(input: String) {
+fn parse_user_input<T: FromStr>(input: String) -> Result<T, <T as FromStr>::Err> {
     return input.parse::<T>();
 }
