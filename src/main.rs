af2d843351801a2f3f80e3cd877f1aaf3c324e5a
sbatial <mail@kbode.com>
2023-06-23 22:28:37 +0200

    improve code according to clippy/bacon

--- src/main.rs
+++ src/main.rs
@@ -32,26 +32,23 @@ fn main() {
 /// This function will return an error if .
 fn wrap_parsing() -> Result<f32, ParseFloatError> {
     let mut rng = thread_rng();
-    let u_in;
 
-    if rng.gen::<usize>() % 100 > 50 {
-        u_in = get_user_input();
+    let u_in = if rng.gen::<usize>() % 100 > 50 {
+        get_user_input()
     } else {
-        u_in = get_faulty_user_input();
-    }
+        get_faulty_user_input()
+    };
 
-    let u_num = parse_user_input::<f32>(u_in);
-    return u_num;
+    parse_user_input::<f32>(u_in)
 }
 
 /// .
 fn get_user_input() -> String {
-    return String::from("32.0");
+    String::from("32.0")
 }
 
 fn get_faulty_user_input() -> String {
-    // return String::from("32..0");
-    return String::from("32..0");
+    String::from("32..0")
 }
 
 /// .
@@ -60,5 +57,5 @@ fn get_faulty_user_input() -> String {
 ///
 /// This function will return an error if .
 fn parse_user_input<T: FromStr>(input: String) -> Result<T, <T as FromStr>::Err> {
-    return input.parse::<T>();
+    input.parse::<T>()
 }
